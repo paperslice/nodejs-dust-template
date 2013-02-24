ba899f13ab97349a2be1b70b73bb8943f83d2a82
Brendan Robinson <brendanrobinson@gmail.com>
2013-02-24 16:46:35 +0000

    Initial template

---
+++ README.md
@@ -0,0 +1,4 @@
+A starting point for trying out nodejs, dust-linked-in templates and backbone.
+
+Install dependencies:       npm install
+Start website on port 5000: foreman start
---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express'), 
+    routes = require('./routes'), 
+    http = require('http'), 
+    path = require('path');
+  
+var dust = require('dustjs-linkedin'),
+    cons = require('consolidate');
+
+var app = express(),
+    port = process.env.PORT || 5000;
+
+app.configure(function(){
+    app.engine('dust', cons.dust);
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'dust');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('your secret here'));
+    app.use(express.session());
+    app.use(app.router);
+    app.use(require('less-middleware')({ src: __dirname + '/public' }));
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log("Express server listening on port " + app.get('port'));
+});
